04dd78c1387859ac9a552d45f0f24d51a2a78496
Maksym Kit <Maksym.Kit@eleks.com>
2022-04-21 19:00:12 +0300

    Wait for multiple connections

--- src/main/java/com/eleks/academy/whoami/App.java
+++ src/main/java/com/eleks/academy/whoami/App.java
@@ -3,6 +3,9 @@ package com.eleks.academy.whoami;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.eleks.academy.whoami.core.Game;
 import com.eleks.academy.whoami.networking.client.ClientPlayer;
@@ -11,13 +14,19 @@ import com.eleks.academy.whoami.networking.server.ServerImpl;
 public class App {
 
 	public static void main(String[] args) throws IOException {
+		int players = readPlayersArg(args);
 
 		ServerImpl server = new ServerImpl(888);
 
 		Game game = server.startGame();
 
-		var socket = server.waitForPlayer(game);
+		List<Socket> playerList = new ArrayList<>(players);
+		for (int i = 0; i < players; i++) {
+			var socket = server.waitForPlayer(game);
+			playerList.add(socket);
+		}
 
+		Socket socket = playerList.get(0);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
 		boolean gameStatus = true;
@@ -41,6 +50,26 @@ public class App {
 		}
 
 		server.stopServer(socket, reader);
+	}
+
+	private static int readPlayersArg(String[] args) {
+		if (args.length < 1) {
+			return 2;
+		} else {
+			try {
+				int players = Integer.parseInt(args[0]);
+				if (players < 2) {
+					return 2;
+				} else if (players > 5) {
+					return 5;
+				} else {
+					return players;
+				}
+			} catch (NumberFormatException e) {
+				System.err.printf("Cannot parse number of players. Assuming 2. (%s)%n", e.getMessage());
+				return 2;
+			}
+		}
 	}
 
 }
