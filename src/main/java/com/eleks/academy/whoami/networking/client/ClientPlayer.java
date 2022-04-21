47225fb7261e12806130795c97092a5dc1ae75f3
Maksym Kit <Maksym.Kit@eleks.com>
2022-04-21 20:13:32 +0300

    Remove unused socket property

--- src/main/java/com/eleks/academy/whoami/networking/client/ClientPlayer.java
+++ src/main/java/com/eleks/academy/whoami/networking/client/ClientPlayer.java
@@ -11,13 +11,11 @@ import com.eleks.academy.whoami.core.Player;
 public class ClientPlayer implements Player {
 
 	private String name;
-	private Socket socket;
 	private BufferedReader reader;
 	private PrintStream writer;
 
 	public ClientPlayer(String name, Socket socket) throws IOException {
 		this.name = name;
-		this.socket = socket;
 		this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		this.writer = new PrintStream(socket.getOutputStream());
 	}
